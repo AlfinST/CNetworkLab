668ff344a88376c1bbca4e8d998c4197697e6422
AlfinST <alfinsthomas@gmail.com>
2019-03-13 19:26:17 +0530

    tiny tweaks

--- SharedMemory/ShmClientz.c
+++ SharedMemory/ShmClientz.c
@@ -8,7 +8,6 @@
 #include <sys/mman.h>
 
 #define SEM_MUTEX_NAME "/sem-mutex"
-#define SEM_BUFFER_COUNT_NAME "/sem-buffer-count"
 #define SEM_SPOOL_SIGNAL_NAME "/sem-spool-signal"
 #define SHARED_MEM_NAME "/posix-shared-mem-example"
 
